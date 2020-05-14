0155f2ee3f1d577064ce077936896769ea32377d
Sahil Narula <sahilnarula98@gmail.com>
2020-05-14 19:01:56 +0530

    Controller Testing Commit

---
+++ backend/src/main/java/com/capstore/app/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.capstore.app.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin(origins = "http://localhost:4200",allowedHeaders = "*")
+public class CustomerController {
+	
+	//random comment
+	// krapiya yha apne functions bana dijiye
+	//mike testing 
+	
+}
