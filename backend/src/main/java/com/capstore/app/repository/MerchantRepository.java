ceeb1e0ddae9f35f0a15d9a5465f01b7201d0ed6
Sahil Narula <sahilnarula98@gmail.com>
2020-05-15 15:54:02 +0530

    removed

--- backend/src/main/java/com/capstore/app/repository/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstore.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capstore.app.models.MerchantDetails;
-
-public interface MerchantRepository extends JpaRepository<MerchantDetails, Integer> {
-
-}
--- backend/src/main/java/com/capstore/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstore.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capstore.app.models.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
