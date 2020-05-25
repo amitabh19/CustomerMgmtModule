985a3362e5646f3afb610038f2690636f9ba41fd
Nikhil Singh <sshwyam@gmail.com>
2020-05-25 22:26:19 +0530

    junit testing of customer details

--- backend/src/test/java/com/capstore/app/customerModule/test/ApplicationTests.java
+++ backend/src/test/java/com/capstore/app/customerModule/test/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.capstore.app.customerModule.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- backend/src/test/java/com/capstore/app/customerModule/test/CustomerUnitTesting.java
+++ backend/src/test/java/com/capstore/app/customerModule/test/CustomerUnitTesting.java
@@ -1,5 +1,6 @@
 package com.capstore.app.customerModule.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -30,6 +31,9 @@ class CustomerUnitTesting {
 
 	@Autowired
 	private CustomerController customerController;
+	
+	@Autowired
+	private CustomerRepository customerRepository;
 
 	
 	//test functions for cart module
@@ -162,6 +166,28 @@ class CustomerUnitTesting {
 		}
 		Assert.assertEquals(true, checked);
 		
+	}
+	
+	//test function to get customer details by id
+	@Test
+	@Rollback(true)
+	public void testGetCustomerDetailById() {
+		CustomerDetails customerDetail = new CustomerDetails("Roxane", "roxy", "roxy3996", "roxy@gmail.com", "User", true, "What is your petname", "Roxy", "3456789012", "2345678901", "rox@gmail.com", "India", null, null, null, null, null);
+		customerRepository.save(customerDetail);
+		CustomerDetails found=null;
+		found = customerController.getCustomerDetailById(customerDetail.getUserId());
+		assertThat(found).isEqualTo(customerDetail);
+	}
+	
+	//test function to update customer details
+	@Test
+	@Rollback(true)
+	public void testUpdateCustomerDetails() {
+		CustomerDetails customerDetail = new CustomerDetails("Roxane", "roxy", "roxy3996", "roxy@gmail.com", "User", true, "What is your petname", "Roxy", "3456789012", "2345678901", "rox@gmail.com", "India", null, null, null, null, null);
+		customerRepository.save(customerDetail);
+		customerDetail.setName("Roxxane");
+		customerRepository.save(customerDetail);
+		assertThat(customerDetail.getName()).isEqualTo("Roxxane");
 	}
 	
 }
